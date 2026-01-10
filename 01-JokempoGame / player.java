52a69298832dcd97bb47f70d7c385552174b2a4d
guizyre <goncalves.guilherme@academico.ifpb.edu.br>
2026-01-10 14:44:46 -0300

    Update  player.java

--- 01-JokempoGame / player.java
+++ 01-JokempoGame / player.java
@@ -1,23 +1,23 @@
 package academy.guih.poo.javacore.classes.dominio;
 import java.util.Random;
+import java.util.Scanner;
 
-public class Player {
+public class Player2 {
     public String nome;
-    public int num_vitorias;
-    public String[] opcoes = {"pedra", "papel", "tesoura"};
+    public String[] jogada = {"pedra", "papel", "tesoura"};
     public String jogada_atual;
+    public int num_vitorias = 0;
 
     public void sorteio(){
         Random random = new Random();
-        int indice = random.nextInt(opcoes.length);
-        this.jogada_atual = opcoes[indice];
+        this.jogada_atual = jogada[random.nextInt(jogada.length)];
     }
 
-    public String aJogada_atual(){
+    public String ultimaJogada(){
         return this.jogada_atual;
     }
 
-    public int incrementar_vitorias(){
-        return this.num_vitorias++;
+    public int incrementar_pontos(){
+        return ++this.num_vitorias;
     }
 }
